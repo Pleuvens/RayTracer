837b9a9960effc6f45f19917822e5163c25afd79
Pleuvens Fervil <pleuvens.fervil@gmail.com>
2020-04-29 01:00:46 +0300

    fix test changes

--- src/main.cc
+++ src/main.cc
@@ -7,9 +7,9 @@ int main() {
     Material     mirror(1.0, Vec4f(0.0, 10.0, 0.8, 0.0), Vec3f(1.0, 1.0, 1.0), 1425.);
     
     Sphere  s(Vec3f(  -3,    0, -16), 2,      ivory);
-    Sphere s1(Vec3f(-1.0, -1.5, -12), 2, red_rubber);
+    Sphere s1(Vec3f(-1.0, -1.5, -12), 2,     mirror);
     Sphere s2(Vec3f( 1.5, -0.5, -18), 3, red_rubber);
-    Sphere s3(Vec3f(   7,    5, -18), 4,      ivory);
+    Sphere s3(Vec3f(   7,    5, -18), 4,      glass);
     std::vector<Sphere> spheres = { s, s1, s2, s3 };
     
     Light  l(Light(Vec3f(-20, 20,  20), 1.5));
--- src/scene.cc
+++ src/scene.cc
@@ -30,7 +30,6 @@ Vec3f Scene::cast_ray(const Vec3f &origin, const Vec3f &dir, size_t depth) const
             !scene_intersect(origin, dir, hit, normal, material))
         return Vec3f(0.2, 0.7, 0.8);
 
-/*
     Vec3f reflect_dir = Vec3f::reflect(dir, normal).normalize();
     Vec3f reflect_origin = Vec3f::sum(reflect_dir, normal) < 0
         ? hit - normal * 1e-3 : hit + normal * 1e+3;
@@ -39,12 +38,12 @@ Vec3f Scene::cast_ray(const Vec3f &origin, const Vec3f &dir, size_t depth) const
     Vec3f refract_origin = Vec3f::sum(refract_dir, normal) < 0
         ? hit - normal * 1e-3 : hit + normal * 1e+3;
     Vec3f refract_color = cast_ray(refract_origin, refract_dir, depth + 1);
-*/
+
     float diffuse_light_intensity = 0;
     float specular_light_intensity = 0;
     for (size_t i = 0; i < lights_.size(); i++) {
         Vec3f light_dir = (lights_[i].position_ - hit).normalize();
-/* 
+
         float light_distance = (lights_[i].position_ - hit).norm();
         Vec3f shadow_origin = Vec3f::sum(light_dir, normal) < 0 ?
             hit - normal * 1e-3 : hit + normal * 1e-3;
@@ -55,17 +54,17 @@ Vec3f Scene::cast_ray(const Vec3f &origin, const Vec3f &dir, size_t depth) const
                     tmp_material)
                 && (shadow_hit - shadow_origin).norm() < light_distance)
             continue;
-*/
+
         diffuse_light_intensity += lights_[i].intensity_ *
             std::max(0.f, Vec3f::sum(light_dir,normal));
         specular_light_intensity +=
             powf(std::max(0.f, Vec3f::sum(Vec3f::reflect(light_dir, normal), dir)),
                     material.specular_) * lights_[i].intensity_;
     }
-    return //material.diffuse_ * diffuse_light_intensity * material.albedo_[0] +
-        Vec3f(1., 1., 1.) * specular_light_intensity * material.albedo_[1];// +
-        //reflect_color * material.albedo_[2] +
-        //refract_color * material.albedo_[3];
+    return material.diffuse_ * diffuse_light_intensity * material.albedo_[0] +
+        Vec3f(1., 1., 1.) * specular_light_intensity * material.albedo_[1] +
+        reflect_color * material.albedo_[2] +
+        refract_color * material.albedo_[3];
 }
 
 void Scene::render() const {
